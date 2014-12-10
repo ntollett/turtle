0a6a95a654f997805daa9c3be7a96509149512e5
Nick Tollett <tollettn@gmail.com>
2014-12-10 14:12:08 -0800

    Fixed it to pass by reference

--- turtle.c
+++ turtle.c
@@ -7,16 +7,16 @@ typedef struct
 	int age;
 } turtle;
 
-void happy_birthday(turtle t)
+void happy_birthday(turtle *t)
 {
-	t.age = t.age + 1;
-	printf("Happy Birthday %s!  You are now %i years old!\n", t.name, t.age);
+	(*t).age = (*t).age + 1;
+	printf("Happy Birthday %s!  You are now %i years old!\n", (*t).name, (*t).age);
 }
 
 int main()
 {
 	turtle myrtle = {"Myrtle", "Leatherback Sea Turtle", 99};
-	happy_birthday(myrtle);
+	happy_birthday(&myrtle);
 	printf("%s's age is now %i\n", myrtle.name, myrtle.age);
 	return 0;
 }
